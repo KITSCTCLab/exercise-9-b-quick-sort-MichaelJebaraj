40f9e06a2da305d1cd67d8c4da9bd866c15086f5
MichaelJebaraj <108014678+MichaelJebaraj@users.noreply.github.com>
2022-10-12 09:35:15 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ def partition(arr,low,high):
         i = i+1
         arr[i],arr[j] = arr[j],arr[i]
         arr[i+1],arr[high] = arr[high],arr[i+1]
-        return ( i+1 )
+    return ( i+1 )
 # sort
 def quick_sort(arr,low,high):
     if low < high:
@@ -20,7 +20,7 @@ def quick_sort(arr,low,high):
 # sort the partitions
     quick_sort(arr, low, pi-1)
     quick_sort(arr, pi+1, high)
-    return arr
+return arr
 
 input_data = input()
 data = []
